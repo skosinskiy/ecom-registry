5b64c82a0793a2733336d02c2aee57ebb094d4d5
Stanislav Kosinskiy <Stanislav.Kosinskiy@alfabank.kiev.ua>
2019-07-02 17:40:26 +0300

    added tests for genericExceptionHandler

--- src/test/java/com/kosinskyi/ecom/registry/exception/GenericExceptionHandlerTest.java
+++ src/test/java/com/kosinskyi/ecom/registry/exception/GenericExceptionHandlerTest.java
@@ -1,6 +1,7 @@
-package com.kosinskyi.ecom.registry.security;
+package com.kosinskyi.ecom.registry.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kosinskyi.ecom.registry.dto.response.error.InternalErrorResponse;
 import com.kosinskyi.ecom.registry.dto.response.error.UnauthorizedErrorResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,8 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -25,16 +25,16 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class JwtAuthenticationEntryPointTest {
+public class GenericExceptionHandlerTest {
 
-	@Autowired
-	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+  @Autowired
+  private GenericExceptionHandler genericExceptionHandler;
 
-	@Autowired
+  @Autowired
   private ObjectMapper objectMapper;
 
-	@Test
-	public void commenceTest() throws IOException {
+  @Test
+  public void handleSecurityExceptionTest() throws IOException {
     String errorMessage = "Bad credentials";
     String path = "/";
     BadCredentialsException badCredentialsException = new BadCredentialsException(errorMessage);
@@ -45,7 +45,7 @@ public class JwtAuthenticationEntryPointTest {
     when(mockRequest.getServletPath()).thenReturn(path);
     when(mockResponse.getWriter()).thenReturn(mockWriter);
 
-    jwtAuthenticationEntryPoint.commence(mockRequest, mockResponse, badCredentialsException);
+    genericExceptionHandler.handleSecurityException(badCredentialsException, mockRequest, mockResponse);
 
     ArgumentCaptor captor = ArgumentCaptor.forClass(String.class);
     verify(mockWriter, times(1))
@@ -61,5 +61,35 @@ public class JwtAuthenticationEntryPointTest {
 
     verify(mockResponse, times(1))
         .sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.name());
-	}
+  }
+
+  @Test
+  public void handleException() throws IOException {
+    String errorMessage = "Runtime exception";
+    String path = "/";
+    RuntimeException badCredentialsException = new RuntimeException(errorMessage);
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    HttpServletResponse mockResponse = mock(HttpServletResponse.class);
+    PrintWriter mockWriter = mock(PrintWriter.class);
+
+    when(mockRequest.getServletPath()).thenReturn(path);
+    when(mockResponse.getWriter()).thenReturn(mockWriter);
+
+    genericExceptionHandler.handleException(badCredentialsException, mockRequest, mockResponse);
+
+    ArgumentCaptor captor = ArgumentCaptor.forClass(String.class);
+    verify(mockWriter, times(1))
+        .write((String) captor.capture());
+    InternalErrorResponse captureErrorResponse =
+        objectMapper.readValue((String) captor.getValue(), InternalErrorResponse.class);
+
+    assertEquals(errorMessage, captureErrorResponse.getMessage());
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.name(), captureErrorResponse.getError());
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), captureErrorResponse.getStatus());
+    assertEquals(path, captureErrorResponse.getPath());
+    assertNotNull(captureErrorResponse.getTimeStamp());
+
+    verify(mockResponse, times(1))
+        .sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.name());
+  }
 }
