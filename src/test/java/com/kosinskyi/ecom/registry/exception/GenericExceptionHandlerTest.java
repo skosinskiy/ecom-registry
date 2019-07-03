a3368f7011857f6a87c143986f303448d1da42c6
Stanislav Kosinskiy <Stanislav.Kosinskiy@alfabank.kiev.ua>
2019-07-03 14:37:00 +0300

    removed unused import

--- src/test/java/com/kosinskyi/ecom/registry/exception/GenericExceptionHandlerTest.java
+++ src/test/java/com/kosinskyi/ecom/registry/exception/GenericExceptionHandlerTest.java
@@ -2,8 +2,6 @@ package com.kosinskyi.ecom.registry.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kosinskyi.ecom.registry.dto.response.ErrorResponse;
-import com.kosinskyi.ecom.registry.dto.response.error.InternalErrorResponse;
-import com.kosinskyi.ecom.registry.dto.response.error.UnauthorizedErrorResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
