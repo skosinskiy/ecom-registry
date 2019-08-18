89710709e859098c1102efb9ba02c3d1d68732f1
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-08-18 21:49:38 +0300

    small fixes

--- src/main/java/com/kosinskyi/ecom/registry/controller/AuthController.java
+++ src/main/java/com/kosinskyi/ecom/registry/controller/AuthController.java
@@ -6,8 +6,6 @@ import com.kosinskyi.ecom.registry.dto.response.LoginResponse;
 import com.kosinskyi.ecom.registry.security.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,13 +25,11 @@ public class AuthController {
   }
 
   @PostMapping
-  @PreAuthorize("permitAll()")
   public ResponseEntity<LoginResponse> generateJwt(@Valid @RequestBody LoginRequest loginRequest) {
     return ResponseEntity.ok(securityService.setAuthenticationAndGenerateJwt(loginRequest));
   }
 
   @PostMapping("refresh")
-  @PreAuthorize("permitAll()")
   public ResponseEntity<LoginResponse> getCurrentUser(@Valid @RequestBody RefreshRequest refreshRequest) {
     return ResponseEntity.ok(securityService.refreshToken(refreshRequest));
   }
